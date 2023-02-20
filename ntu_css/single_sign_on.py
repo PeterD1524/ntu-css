7165aa4425758810bf0d706baa3891406ddeeddc
PeterD1524 <qaz246135@gmail.com>
2023-02-20 20:20:36 +0800

    form fields

--- ntu_css/single_sign_on.py
+++ ntu_css/single_sign_on.py
@@ -1,7 +1,20 @@
 import urllib.parse
 
+import lxml.html
+
 import ntu_css.http
 import ntu_css.utils
+
+
+def form_fields(form: lxml.html.FormElement):
+    for key in form.fields:
+        assert isinstance(key, str)
+        value = form.fields[key]
+        if value is None:
+            value = None
+        else:
+            assert isinstance(value, str)
+        yield (key, value)
 
 
 def login(response: ntu_css.http.Response, username: str, password: str):
@@ -12,7 +25,7 @@ def login(response: ntu_css.http.Response, username: str, password: str):
     (form,) = ntu_css.utils.assert_list_of_form_element(
         document.xpath('//*[@id="content"]/form[@name="p1"]')
     )
-    data = dict(form.fields)
+    data = dict(form_fields(form))
     assert "user" in data
     data["user"] = username
     assert "pass" in data
@@ -20,6 +33,7 @@ def login(response: ntu_css.http.Response, username: str, password: str):
     action = ntu_css.utils.assert_str(form.action)
     method = ntu_css.utils.assert_str(form.method)
     assert method == "POST"
+    data = {k: ntu_css.utils.assert_str(v) for k, v in data.items()}
     return ntu_css.http.Request(
         method=method, url=urllib.parse.urljoin(response.url(), action), data=data
     )
